3aa1dd2d06904628e8c6ec73f2cabe0d79ae8e5d
action_bot <potterli520@gmail.com>
2022-01-08 04:14:24 +0800

    fix bug

--- proxy/server/config.go
+++ proxy/server/config.go
@@ -4,8 +4,9 @@ import (
 	"github.com/Potterli20/trojan-go-fork/config"
 	"github.com/Potterli20/trojan-go-fork/proxy/client"
 
+
 func init() {
-	config.RegisterConfigCreator(Name, func() interface{} {
-		return new(client.Config)
-	})
+		config.RegisterConfigCreator(Name, func() interface{} {
+			return new(client.Config)
+		})
 }
