24b18e7937701116cf900d59e40dc35e25eac320
trli <potterli520@gmail.com>
2022-08-05 12:53:21 +0800

    Update server.go

--- tunnel/mux/server.go
+++ tunnel/mux/server.go
@@ -3,7 +3,7 @@ package mux
 import (
 	"context"
 
-	"github.com/xtaci/smux"
+	"github.com/potterli20/smux"
 
 	"github.com/Potterli20/trojan-go-fork/common"
 	"github.com/Potterli20/trojan-go-fork/log"
