d069c40265cf2bcda1fad5a394166e318126330c
trli <potterli520@gmail.com>
2022-09-08 11:21:28 +0800

    Update proxy_test.go

--- test/scenario/proxy_test.go
+++ test/scenario/proxy_test.go
@@ -12,7 +12,7 @@ import (
 	"time"
 
 	netproxy "golang.org/x/net/proxy"
-	"google.golang.org/genproto/googleapis/cloud/oslogin/v1alpha"
+	"google.golang.org/genproto/googleapis/cloud/oslogin/v1"
 
 	_ "github.com/Potterli20/trojan-go-fork/api"
 	_ "github.com/Potterli20/trojan-go-fork/api/service"
