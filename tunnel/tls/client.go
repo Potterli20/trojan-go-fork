387724b5b9adcef9cbe63b66766285a4a59ab057
root <potter@trli.club>
2023-11-09 12:27:05 +0800

    update tls

--- tunnel/tls/client.go
+++ tunnel/tls/client.go
@@ -68,37 +68,14 @@ func (c *Client) DialConn(_ *tunnel.Address, overlay tunnel.Tunnel) (tunnel.Conn
 func NewClient(ctx context.Context, underlay tunnel.Client) (*Client, error) {
 	cfg := config.FromContext(ctx, Name).(*Config)
 
-	helloID := tls.ClientHelloID{}
-	// keep the parameter name consistent with upstream
-	// https://github.com/refraction-networking/utls/blob/35e5b05fc4b6f8c4351d755f2570bc293f30aaf6/u_common.go#L114-L132
-	if cfg.TLS.Fingerprint != "" {
-		switch strings.ToLower(cfg.TLS.Fingerprint) {
-		case "chrome":
-			helloID = tls.HelloChrome_Auto
-		case "ios":
-			helloID = tls.HelloIOS_Auto
-		case "firefox":
-			helloID = tls.HelloFirefox_Auto
-		case "edge":
-			helloID = tls.HelloEdge_Auto
-		case "safari":
-			helloID = tls.HelloSafari_Auto
-		case "360browser":
-			helloID = tls.Hello360_Auto
-		case "qqbrowser":
-			helloID = tls.HelloQQ_Auto
-		default:
-			return nil, common.NewError("Invalid 'fingerprint' value in configuration: '" + cfg.TLS.Fingerprint + "'. Possible values are 'chrome' (default), 'ios', 'firefox', 'edge', 'safari', '360browser', or 'qqbrowser'.")
-		}
-		log.Info("Your trojan's TLS fingerprint will look like", cfg.TLS.Fingerprint)
-	} else {
-		helloID = tls.HelloChrome_Auto
-		log.Info("No 'fingerprint' value specified in your configuration. Your trojan's TLS fingerprint will look like Chrome by default.")
+	helloID, err := getHelloID(cfg.TLS.Fingerprint)
+	if err != nil {
+		return nil, err
 	}
 
 	if cfg.TLS.SNI == "" {
 		cfg.TLS.SNI = cfg.RemoteHost
-		log.Warn("tls sni is unspecified")
+		log.Warn("tls sni is unspecified, it's recommended to specify it for better security")
 	}
 
 	client := &Client{
@@ -112,40 +89,72 @@ func NewClient(ctx context.Context, underlay tunnel.Client) (*Client, error) {
 	}
 
 	if cfg.TLS.CertPath != "" {
-		caCertByte, err := ioutil.ReadFile(cfg.TLS.CertPath)
+		err := loadCert(client, cfg.TLS.CertPath)
 		if err != nil {
-			return nil, common.NewError("failed to load cert file").Base(err)
+			return nil, err
 		}
-		client.ca = x509.NewCertPool()
-		ok := client.ca.AppendCertsFromPEM(caCertByte)
-		if !ok {
-			log.Warn("invalid cert list")
-		}
-		log.Info("using custom cert")
-
-		// print cert info
-		pemCerts := caCertByte
-		for len(pemCerts) > 0 {
-			var block *pem.Block
-			block, pemCerts = pem.Decode(pemCerts)
-			if block == nil {
-				break
-			}
-			if block.Type != "CERTIFICATE" || len(block.Headers) != 0 {
-				continue
-			}
-			cert, err := x509.ParseCertificate(block.Bytes)
-			if err != nil {
-				continue
-			}
-			log.Trace("issuer:", cert.Issuer, "subject:", cert.Subject)
-		}
-	}
-
-	if cfg.TLS.CertPath == "" {
+	} else {
 		log.Info("cert is unspecified, using default ca list")
 	}
 
 	log.Debug("tls client created")
 	return client, nil
+}
+
+func getHelloID(fingerprint string) (tls.ClientHelloID, error) {
+	fingerprints := map[string]tls.ClientHelloID{
+		"chrome":     tls.HelloChrome_Auto,
+		"ios":        tls.HelloIOS_Auto,
+		"firefox":    tls.HelloFirefox_Auto,
+		"edge":       tls.HelloEdge_Auto,
+		"safari":     tls.HelloSafari_Auto,
+		"360browser": tls.Hello360_Auto,
+		"qqbrowser":  tls.HelloQQ_Auto,
+	}
+
+	if fingerprint == "" {
+		log.Info("No 'fingerprint' value specified in your configuration. Your trojan's TLS fingerprint will look like Chrome by default.")
+		return tls.HelloChrome_Auto, nil
+	}
+
+	helloID, ok := fingerprints[strings.ToLower(fingerprint)]
+	if !ok {
+		return tls.ClientHelloID{}, common.NewError("Invalid 'fingerprint' value in configuration: '" + fingerprint + "'. Possible values are 'chrome' (default), 'ios', 'firefox', 'edge', 'safari', '360browser', or 'qqbrowser'.")
+	}
+
+	log.Info("Your trojan's TLS fingerprint will look like", fingerprint)
+	return helloID, nil
+}
+
+func loadCert(client *Client, certPath string) error {
+	caCertByte, err := ioutil.ReadFile(certPath)
+	if err != nil {
+		return common.NewError("failed to load cert file at path: " + certPath).Base(err)
+	}
+	client.ca = x509.NewCertPool()
+	ok := client.ca.AppendCertsFromPEM(caCertByte)
+	if !ok {
+		log.Warn("invalid cert list")
+	}
+	log.Info("using custom cert")
+
+	// print cert info
+	pemCerts := caCertByte
+	for len(pemCerts) > 0 {
+		var block *pem.Block
+		block, pemCerts = pem.Decode(pemCerts)
+		if block == nil {
+			break
+		}
+		if block.Type != "CERTIFICATE" || len(block.Headers) != 0 {
+			continue
+		}
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			continue
+		}
+		log.Trace("issuer:", cert.Issuer, "subject:", cert.Subject)
+	}
+
+	return nil
 }
