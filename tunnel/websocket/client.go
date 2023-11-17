23753ab3be3303fed88e3fda146eddf2a11ca1d0
trli <potterli520@gmail.com>
2023-11-17 14:37:04 +0800

    Update client.go

--- tunnel/websocket/client.go
+++ tunnel/websocket/client.go
@@ -1,3 +1,5 @@
+// tunnel/websocket/client.go
+
 package websocket
 
 import (
@@ -8,34 +10,7 @@ import (
 	"github.com/Potterli20/trojan-go-fork/tunnel"
 	"golang.org/x/net/websocket"
 	"strings"
-	"time"
 )
-
-// OutboundConn represents a WebSocket connection.
-type OutboundConn struct {
-	websocket *websocket.Conn
-	tcpConn   tunnel.Conn
-}
-
-// SetReadDeadline sets the read deadline on the connection.
-func (c *OutboundConn) SetReadDeadline(t time.Time) error {
-	return c.websocket.SetReadDeadline(t)
-}
-
-// Read implements the Read method of the tunnel.Conn interface.
-func (c *OutboundConn) Read(b []byte) (n int, err error) {
-	return c.websocket.Read(b)
-}
-
-// Write implements the Write method of the tunnel.Conn interface.
-func (c *OutboundConn) Write(b []byte) (n int, err error) {
-	return c.websocket.Write(b)
-}
-
-// Close implements the Close method of the tunnel.Conn interface.
-func (c *OutboundConn) Close() error {
-	return c.websocket.Close()
-}
 
 // Client is a WebSocket client.
 type Client struct {
@@ -62,7 +37,6 @@ func (c *Client) DialConn(addr *tunnel.Address, t tunnel.Tunnel) (tunnel.Conn, e
 	}
 	return &OutboundConn{
 		websocket: wsConn,
-		tcpConn:   conn,
 	}, nil
 }
 
