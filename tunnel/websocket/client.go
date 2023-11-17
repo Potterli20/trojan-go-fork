66a21528eeafcc85b07625d53861460f254f8408
trli <potterli520@gmail.com>
2023-11-17 14:40:14 +0800

    Update client.go

--- tunnel/websocket/client.go
+++ tunnel/websocket/client.go
@@ -1,66 +1,62 @@
-// tunnel/websocket/client.go
-
 package websocket
 
 import (
 	"context"
+	"strings"
+
+	"golang.org/x/net/websocket"
+
 	"github.com/Potterli20/trojan-go-fork/common"
 	"github.com/Potterli20/trojan-go-fork/config"
 	"github.com/Potterli20/trojan-go-fork/log"
 	"github.com/Potterli20/trojan-go-fork/tunnel"
-	"golang.org/x/net/websocket"
-	"strings"
 )
 
-// Client is a WebSocket client.
 type Client struct {
 	underlay tunnel.Client
 	hostname string
 	path     string
 }
 
-// DialConn dials a connection.
-func (c *Client) DialConn(addr *tunnel.Address, t tunnel.Tunnel) (tunnel.Conn, error) {
+func (c *Client) DialConn(*tunnel.Address, tunnel.Tunnel) (tunnel.Conn, error) {
 	conn, err := c.underlay.DialConn(nil, &Tunnel{})
 	if err != nil {
-		return nil, common.NewError("WebSocket cannot dial with underlying client").Base(err)
+		return nil, common.NewError("websocket cannot dial with underlying client").Base(err)
 	}
 	url := "wss://" + c.hostname + c.path
 	origin := "https://" + c.hostname
 	wsConfig, err := websocket.NewConfig(url, origin)
 	if err != nil {
-		return nil, common.NewError("Invalid WebSocket config").Base(err)
+		return nil, common.NewError("invalid websocket config").Base(err)
 	}
 	wsConn, err := websocket.NewClient(wsConfig, conn)
 	if err != nil {
-		return nil, common.NewError("WebSocket failed to handshake with the server").Base(err)
+		return nil, common.NewError("websocket failed to handshake with server").Base(err)
 	}
 	return &OutboundConn{
-		websocket: wsConn,
+		Conn:    wsConn,
+		tcpConn: conn,
 	}, nil
 }
 
-// DialPacket dials a packet connection.
-func (c *Client) DialPacket(t tunnel.Tunnel) (tunnel.PacketConn, error) {
-	return nil, common.NewError("Not supported by WebSocket")
+func (c *Client) DialPacket(tunnel.Tunnel) (tunnel.PacketConn, error) {
+	return nil, common.NewError("not supported by websocket")
 }
 
-// Close closes the WebSocket client.
 func (c *Client) Close() error {
 	return c.underlay.Close()
 }
 
-// NewClient creates a new WebSocket client.
 func NewClient(ctx context.Context, underlay tunnel.Client) (*Client, error) {
-	cfg := config.FromContext(ctx, "websocket").(*Config)
+	cfg := config.FromContext(ctx, Name).(*Config)
 	if !strings.HasPrefix(cfg.Websocket.Path, "/") {
-		return nil, common.NewError("WebSocket path must start with \"/\"")
+		return nil, common.NewError("websocket path must start with \"/\"")
 	}
 	if cfg.Websocket.Host == "" {
 		cfg.Websocket.Host = cfg.RemoteHost
-		log.Warn("Empty WebSocket hostname")
+		log.Warn("empty websocket hostname")
 	}
-	log.Debug("WebSocket client created")
+	log.Debug("websocket client created")
 	return &Client{
 		hostname: cfg.Websocket.Host,
 		path:     cfg.Websocket.Path,
