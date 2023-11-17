2c1899650574bd5570ed472c3e9dd1c825f44929
trli <potterli520@gmail.com>
2023-11-17 14:30:16 +0800

    Update client.go

--- tunnel/websocket/client.go
+++ tunnel/websocket/client.go
@@ -8,7 +8,39 @@ import (
 	"github.com/Potterli20/trojan-go-fork/tunnel"
 	"golang.org/x/net/websocket"
 	"strings"
+	"time"
 )
+
+// OutboundConn represents a WebSocket connection.
+type OutboundConn struct {
+	websocket *websocket.Conn
+	tcpConn   tunnel.Conn
+}
+
+// SetReadDeadline sets the read deadline on the connection.
+func (c *OutboundConn) SetReadDeadline(t time.Time) error {
+	return c.websocket.SetReadDeadline(t)
+}
+
+// Read implements the Read method of the tunnel.Conn interface.
+func (c *OutboundConn) Read(b []byte) (n int, err error) {
+	return c.websocket.Read(b)
+}
+
+// Write implements the Write method of the tunnel.Conn interface.
+func (c *OutboundConn) Write(b []byte) (n int, err error) {
+	return c.websocket.Write(b)
+}
+
+// Close implements the Close method of the tunnel.Conn interface.
+func (c *OutboundConn) Close() error {
+	return c.websocket.Close()
+}
+
+// Tunnel is a placeholder type. You should replace it with your actual implementation.
+type Tunnel struct {
+	// Your implementation details...
+}
 
 // Client is a WebSocket client.
 type Client struct {
@@ -34,8 +66,8 @@ func (c *Client) DialConn(addr *tunnel.Address, t tunnel.Tunnel) (tunnel.Conn, e
 		return nil, common.NewError("WebSocket failed to handshake with server").Base(err)
 	}
 	return &OutboundConn{
-		Conn:    wsConn,
-		tcpConn: conn,
+		websocket: wsConn,
+		tcpConn:   conn,
 	}, nil
 }
 
