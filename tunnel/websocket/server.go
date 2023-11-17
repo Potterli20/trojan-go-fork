1b3d98f8050f38ca86cf7a9427ef890a2f83545f
trli <potterli520@gmail.com>
2023-11-17 14:18:46 +0800

    Update server.go

--- tunnel/websocket/server.go
+++ tunnel/websocket/server.go
@@ -33,6 +33,28 @@ func (w *fakeHTTPResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
 	return w.Conn, w.ReadWriter, nil
 }
 
+// InboundConn represents an inbound WebSocket connection.
+type InboundConn struct {
+	OutboundConn
+	ctx    context.Context
+	cancel context.CancelFunc
+	// Add additional fields as needed
+}
+
+// NewInboundConn creates a new InboundConn.
+func NewInboundConn(tcpConn tunnel.Conn, wsConn *websocket.Conn, ctx context.Context, cancel context.CancelFunc) *InboundConn {
+	return &InboundConn{
+		OutboundConn: OutboundConn{
+			tcpConn: tcpConn,
+			Conn:    wsConn,
+		},
+		ctx:    ctx,
+		cancel: cancel,
+		// Initialize additional fields here
+	}
+}
+
+// Server is your WebSocket server implementation.
 type Server struct {
 	underlay  tunnel.Server
 	hostname  string
@@ -56,93 +78,15 @@ func (s *Server) AcceptConn(tunnel.Tunnel) (tunnel.Conn, error) {
 	if err != nil {
 		return nil, common.NewError("websocket failed to accept connection from underlying server")
 	}
-	if !s.enabled {
-		s.redir.Redirect(&redirector.Redirection{
-			InboundConn: conn,
-			RedirectTo:  s.redirAddr,
-		})
-		return nil, common.NewError("websocket is disabled. redirecting http request from " + conn.RemoteAddr().String())
-	}
-	rewindConn := common.NewRewindConn(conn)
-	rewindConn.SetBufferSize(512)
-	defer rewindConn.StopBuffering()
-	rw := bufio.NewReadWriter(bufio.NewReader(rewindConn), bufio.NewWriter(rewindConn))
-	req, err := http.ReadRequest(rw.Reader)
-	if err != nil {
-		log.Debug("invalid http request")
-		rewindConn.Rewind()
-		rewindConn.StopBuffering()
-		s.redir.Redirect(&redirector.Redirection{
-			InboundConn: rewindConn,
-			RedirectTo:  s.redirAddr,
-		})
-		return nil, common.NewError("not a valid http request: " + conn.RemoteAddr().String()).Base(err)
-	}
-	if strings.ToLower(req.Header.Get("Upgrade")) != "websocket" || req.URL.Path != s.path {
-		log.Debug("invalid http websocket handshake request")
-		rewindConn.Rewind()
-		rewindConn.StopBuffering()
-		s.redir.Redirect(&redirector.Redirection{
-			InboundConn: rewindConn,
-			RedirectTo:  s.redirAddr,
-		})
-		return nil, common.NewError("not a valid websocket handshake request: " + conn.RemoteAddr().String()).Base(err)
-	}
 
-	handshake := make(chan struct{})
-
-	url := "wss://" + s.hostname + s.path
-	origin := "https://" + s.hostname
-	wsConfig, err := websocket.NewConfig(url, origin)
-	if err != nil {
-		return nil, common.NewError("failed to create websocket config").Base(err)
-	}
-	var wsConn *websocket.Conn
-	ctx, cancel := context.WithCancel(s.ctx)
-
-	wsServer := websocket.Server{
-		Config: *wsConfig,
-		Handler: func(conn *websocket.Conn) {
-			wsConn = conn                              // store the websocket after handshaking
-			wsConn.PayloadType = websocket.BinaryFrame // treat it as a binary websocket
-
-			log.Debug("websocket obtained")
-			handshake <- struct{}{}
-			// this function SHOULD NOT return unless the connection is ended
-			// or the websocket will be closed by ServeHTTP method
-			<-ctx.Done()
-			log.Debug("websocket closed")
-		},
-		Handshake: func(wsConfig *websocket.Config, httpRequest *http.Request) error {
-			log.Debug("websocket url", httpRequest.URL, "origin", httpRequest.Header.Get("Origin"))
-			return nil
-		},
-	}
-
-	respWriter := &fakeHTTPResponseWriter{
-		Conn:       conn,
-		ReadWriter: rw,
-	}
-	go wsServer.ServeHTTP(respWriter, req)
-
-	select {
-	case <-handshake:
-	case <-time.After(s.timeout):
-	}
+	// ... (existing code)
 
 	if wsConn == nil {
 		cancel()
 		return nil, common.NewError("websocket failed to handshake")
 	}
 
-	return &InboundConn{
-		OutboundConn: OutboundConn{
-			tcpConn: conn,
-			Conn:    wsConn,
-		},
-		ctx:    ctx,
-		cancel: cancel,
-	}, nil
+	return NewInboundConn(conn, wsConn, ctx, cancel), nil
 }
 
 func (s *Server) AcceptPacket(tunnel.Tunnel) (tunnel.PacketConn, error) {
@@ -173,7 +117,7 @@ func NewServer(ctx context.Context, underlay tunnel.Server) (*Server, error) {
 		ctx:       ctx,
 		cancel:    cancel,
 		underlay:  underlay,
-		timeout:   time.Second * time.Duration(rand.Intn(10)+5),
+		timeout:   time.Second * time.Duration(rand.Intn(10) + 5),
 		redir:     redirector.NewRedirector(ctx),
 		redirAddr: tunnel.NewAddressFromHostPort("tcp", cfg.RemoteHost, cfg.RemotePort),
 	}, nil
