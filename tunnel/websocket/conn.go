8499c5c61d06faa5285513d09eab648ed10d3662
trli <potterli520@gmail.com>
2023-11-17 14:40:47 +0800

    Update conn.go

--- tunnel/websocket/conn.go
+++ tunnel/websocket/conn.go
@@ -1,35 +1,34 @@
 package websocket
 
 import (
+	"context"
 	"net"
-	"time"
 
 	"github.com/Potterli20/trojan-go-fork/tunnel"
 	"golang.org/x/net/websocket"
 )
 
-
-// OutboundConn represents a WebSocket connection.
 type OutboundConn struct {
-	websocket *websocket.Conn
+	*websocket.Conn
+	tcpConn net.Conn
 }
 
-// SetReadDeadline sets the read deadline on the connection.
-func (c *OutboundConn) SetReadDeadline(t time.Time) error {
-	return c.websocket.SetReadDeadline(t)
+func (c *OutboundConn) Metadata() *tunnel.Metadata {
+	return nil
 }
 
-// Read implements the Read method of the tunnel.Conn interface.
-func (c *OutboundConn) Read(b []byte) (n int, err error) {
-	return c.websocket.Read(b)
+func (c *OutboundConn) RemoteAddr() net.Addr {
+	// override RemoteAddr of websocket.Conn, or it will return some url from "Origin"
+	return c.tcpConn.RemoteAddr()
 }
 
-// Write implements the Write method of the tunnel.Conn interface.
-func (c *OutboundConn) Write(b []byte) (n int, err error) {
-	return c.websocket.Write(b)
+type InboundConn struct {
+	OutboundConn
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
-// Close implements the Close method of the tunnel.Conn interface.
-func (c *OutboundConn) Close() error {
-	return c.websocket.Close()
+func (c *InboundConn) Close() error {
+	c.cancel()
+	return c.Conn.Close()
 }
