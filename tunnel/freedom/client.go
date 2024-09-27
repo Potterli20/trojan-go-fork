908fb3e90737c9dcba83c70ac8c9a9de47338c7a
trli <potter@trli.club>
2024-09-27 21:47:42 +0800

    update

--- tunnel/freedom/client.go
+++ tunnel/freedom/client.go
@@ -1,154 +1,139 @@
 package freedom
 
 import (
-	"context"
-	"net"
+    "context"
+    "net"
 
-	"github.com/Potterli20/socks5-fork"
-	"golang.org/x/net/proxy"
+    "github.com/Potterli20/socks5-fork"
+    "golang.org/x/net/proxy"
 
-	"github.com/Potterli20/trojan-go-fork/common"
-	"github.com/Potterli20/trojan-go-fork/config"
-	"github.com/Potterli20/trojan-go-fork/tunnel"
-	dialer_sing_box "github.com/sagernet/sing-box/common/dialer"
-	"github.com/sagernet/sing/common/metadata"
-	"github.com/sagernet/tfo-go"
+    "github.com/Potterli20/trojan-go-fork/common"
+    "github.com/Potterli20/trojan-go-fork/config"
+    "github.com/Potterli20/trojan-go-fork/tunnel"
+    dialer_sing_box "github.com/sagernet/sing-box/common/dialer"
+    "github.com/sagernet/sing/common/metadata"
+    "github.com/sagernet/tfo-go"
 )
 
 type Client struct {
-	preferIPv4   bool
-	noDelay      bool
-	keepAlive    bool
-	ctx          context.Context
-	cancel       context.CancelFunc
-	forwardProxy bool
-	proxyAddr    *tunnel.Address
-	username     string
-	password     string
+    preferIPv4   bool
+    noDelay      bool
+    keepAlive    bool
+    ctx          context.Context
+    cancel       context.CancelFunc
+    forwardProxy bool
+    proxyAddr    *tunnel.Address
+    username     string
+    password     string
 }
 
 func (c *Client) DialConn(addr *tunnel.Address, _ tunnel.Tunnel) (tunnel.Conn, error) {
-	// forward proxy
+    // forward proxy
 
-	if c.forwardProxy {
-		var auth *proxy.Auth
-		if c.username != "" {
-			auth = &proxy.Auth{
-				User:     c.username,
-				Password: c.password,
-			}
-		}
-		dialer, err := proxy.SOCKS5("tcp", c.proxyAddr.String(), auth, proxy.Direct)
-		if err != nil {
-			return nil, common.NewError("freedom failed to init socks dialer")
-		}
-		conn, err := dialer.Dial("tcp", addr.String())
-		if err != nil {
-			return nil, common.NewError("freedom failed to dial target address via socks proxy " + addr.String()).Base(err)
-		}
-		// conn, err := dialer_sing_box.DialSlowContext(&tfo.Dialer{
-		// 	Dialer: net.Dialer{},
-		// }, context.Background(), "tcp", metadata.ParseSocksaddr(addr.String()))
-		// if err != nil {
-		// 	return nil, common.NewError("freedom failed to dial target address via socks proxy " + addr.String()).Base(err)
-		// }
-		return &Conn{
-			Conn: conn,
-		}, nil
-	}
-	network := "tcp"
-	if c.preferIPv4 {
-		network = "tcp4"
-	}
-	// dialer := new(net.Dialer)
-	// tcpConn, err := dialer.DialContext(c.ctx, network, addr.String())
-	// if err != nil {
-	// 	return nil, common.NewError("freedom failed to dial " + addr.String()).Base(err)
-	// }
-	tcpConn, err := dialer_sing_box.DialSlowContext(&tfo.Dialer{
-		Dialer: net.Dialer{
-			DualStack: true,
-		},
-		DisableTFO: false,
-	}, context.Background(), network, metadata.ParseSocksaddr(addr.String()))
-	if err != nil {
-		return nil, common.NewError("freedom failed to dial " + addr.String()).Base(err)
-	}
+    if c.forwardProxy {
+        var auth *proxy.Auth
+        if c.username != "" {
+            auth = &proxy.Auth{
+                User:     c.username,
+                Password: c.password,
+            }
+        }
+        dialer, err := proxy.SOCKS5("tcp", c.proxyAddr.String(), auth, proxy.Direct)
+        if err != nil {
+            return nil, common.NewError("freedom failed to init socks dialer")
+        }
+        conn, err := dialer.Dial("tcp", addr.String())
+        if err != nil {
+            return nil, common.NewError("freedom failed to dial target address via socks proxy " + addr.String()).Base(err)
+        }
+        return &Conn{
+            Conn: conn,
+        }, nil
+    }
+    network := "tcp"
+    if c.preferIPv4 {
+        network = "tcp4"
+    }
+    tcpConn, err := dialer_sing_box.DialSlowContext(&tfo.Dialer{
+        Dialer: net.Dialer{
+            DualStack: true,
+        },
+        DisableTFO: false,
+    }, context.Background(), network, metadata.ParseSocksaddr(addr.String()))
+    if err != nil {
+        return nil, common.NewError("freedom failed to dial " + addr.String()).Base(err)
+    }
 
-	// tcpConn.(*net.TCPConn).SetKeepAlive(c.keepAlive)
-	// tcpConn.(*net.TCPConn).SetNoDelay(c.noDelay)
-	return &Conn{
-		Conn: tcpConn,
-	}, nil
+    return &Conn{
+        Conn: tcpConn,
+    }, nil
 }
 
 func (c *Client) DialPacket(tunnel.Tunnel) (tunnel.PacketConn, error) {
-	if c.forwardProxy {
-		socksClient, err := socks5.NewClient(c.proxyAddr.String(), c.username, c.password, 0, 0)
-		common.Must(err)
-		if err := socksClient.Negotiate(&net.TCPAddr{}); err != nil {
-			return nil, common.NewError("freedom failed to negotiate socks").Base(err)
-		}
-		a, addr, port, err := socks5.ParseAddress("1.1.1.1:53") // useless address
-		common.Must(err)
-		resp, err := socksClient.Request(socks5.NewRequest(socks5.CmdUDP, a, addr, port))
-		if err != nil {
-			return nil, common.NewError("freedom failed to dial udp to socks").Base(err)
-		}
-		// TODO fix hardcoded localhost
-		packetConn, err := net.ListenPacket("udp", "0.0.0.0:0")
-		if err != nil {
-			return nil, common.NewError("freedom failed to listen udp").Base(err)
-		}
-		socksAddr, err := net.ResolveUDPAddr("udp", resp.Address())
-		if err != nil {
-			return nil, common.NewError("freedom recv invalid socks bind addr").Base(err)
-		}
-		if socksAddr.IP.Equal(net.IPv4zero) {
-			ip, err := c.proxyAddr.ResolveIP()
-			if err != nil {
-				return nil, common.NewError("freedom failed to resolve ip").Base(err)
-			}
-			socksAddr.IP = ip
-		}
-		// fmt.Printf("socksAddr: %v\n", socksAddr)
-		return &SocksPacketConn{
-			PacketConn:  packetConn,
-			socksAddr:   socksAddr,
-			socksClient: socksClient,
-		}, nil
-	}
-	network := "udp"
-	if c.preferIPv4 {
-		network = "udp4"
-	}
-	udpConn, err := net.ListenPacket(network, "")
-	if err != nil {
-		return nil, common.NewError("freedom failed to listen udp socket").Base(err)
-	}
-	return &PacketConn{
-		UDPConn: udpConn.(*net.UDPConn),
-	}, nil
+    if c.forwardProxy {
+        socksClient, err := socks5.NewClient(c.proxyAddr.String(), c.username, c.password, 0, 0)
+        common.Must(err)
+        if err := socksClient.Negotiate(&net.TCPAddr{}); err != nil {
+            return nil, common.NewError("freedom failed to negotiate socks").Base(err)
+        }
+        a, addr, port, err := socks5.ParseAddress("1.1.1.1:53") // useless address
+        common.Must(err)
+        resp, err := socksClient.Request(socks5.NewRequest(socks5.CmdUDP, a, addr, port))
+        if err != nil {
+            return nil, common.NewError("freedom failed to dial udp to socks").Base(err)
+        }
+        packetConn, err := net.ListenPacket("udp", "0.0.0.0:0")
+        if err != nil {
+            return nil, common.NewError("freedom failed to listen udp").Base(err)
+        }
+        socksAddr, err := net.ResolveUDPAddr("udp", resp.Address())
+        if err != nil {
+            return nil, common.NewError("freedom recv invalid socks bind addr").Base(err)
+        }
+        if socksAddr.IP.Equal(net.IPv4zero) {
+            ip, err := c.proxyAddr.ResolveIP()
+            if err != nil {
+                return nil, common.NewError("freedom failed to resolve ip").Base(err)
+            }
+            socksAddr.IP = ip
+        }
+        return &SocksPacketConn{
+            PacketConn:  packetConn,
+            socksAddr:   socksAddr,
+            socksClient: socksClient,
+        }, nil
+    }
+    network := "udp"
+    if c.preferIPv4 {
+        network = "udp4"
+    }
+    udpConn, err := net.ListenPacket(network, "")
+    if err != nil {
+        return nil, common.NewError("freedom failed to listen udp socket").Base(err)
+    }
+    return &PacketConn{
+        UDPConn: udpConn.(*net.UDPConn),
+    }, nil
 }
 
 func (c *Client) Close() error {
-	c.cancel()
-	return nil
+    c.cancel()
+    return nil
 }
 
 func NewClient(ctx context.Context, _ tunnel.Client) (*Client, error) {
-	cfg := config.FromContext(ctx, Name).(*Config)
-	addr := tunnel.NewAddressFromHostPort("tcp", cfg.ForwardProxy.ProxyHost, cfg.ForwardProxy.ProxyPort)
-	ctx, cancel := context.WithCancel(ctx)
-	return &Client{
-		ctx:          ctx,
-		cancel:       cancel,
-		noDelay:      cfg.TCP.NoDelay,
-		keepAlive:    cfg.TCP.KeepAlive,
-		preferIPv4:   cfg.TCP.PreferIPV4,
-		forwardProxy: cfg.ForwardProxy.Enabled,
-		proxyAddr:    addr,
-		username:     cfg.ForwardProxy.Username,
-		password:     cfg.ForwardProxy.Password,
-	}, nil
+    cfg := config.FromContext(ctx, Name).(*Config)
+    addr := tunnel.NewAddressFromHostPort("tcp", cfg.ForwardProxy.ProxyHost, cfg.ForwardProxy.ProxyPort)
+    ctx, cancel := context.WithCancel(ctx)
+    return &Client{
+        ctx:          ctx,
+        cancel:       cancel,
+        noDelay:      cfg.TCP.NoDelay,
+        keepAlive:    cfg.TCP.KeepAlive,
+        preferIPv4:   cfg.TCP.PreferIPV4,
+        forwardProxy: cfg.ForwardProxy.Enabled,
+        proxyAddr:    addr,
+        username:     cfg.ForwardProxy.Username,
+        password:     cfg.ForwardProxy.Password,
+    }, nil
 }
