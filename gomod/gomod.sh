7f05b8259624000c7f74dc76b30565278620514d
trli <potter@trli.club>
2025-05-03 13:02:19 +0800

    重命名 go 步骤为 go-modules，并添加 gomod.sh 脚本以获取最新的依赖项

---
+++ gomod/gomod.sh
@@ -0,0 +1,14 @@
+# 获取 tfo-go 最新的 commits
+tfo_commit_hash=$(curl -s https://api.github.com/repos/database64128/tfo-go/commits | grep "sha" | head -n 1 | cut -d '"' -f 4)
+# 使用提取的 commit hash 通过 go get 获取 tfo-go
+go get github.com/database64128/tfo-go/v2@$tfo_commit_hash
+
+# 获取 xray 最新的 commits
+xray_commit_hash=$(curl -s https://api.github.com/repos/XTLS/Xray-core/commits | grep "sha" | head -n 1 | cut -d '"' -f 4)
+# 使用提取的 commit hash 通过 go get 获取 tfo-go
+go get github.com/XTLS/Xray-core@$xray_commit_hash
+
+# 获取 gorm 最新的 commits
+gorm_commit_hash=$(curl -s https://api.github.com/repos/go-gorm/gorm/commits | grep "sha" | head -n 1 | cut -d '"' -f 4)
+# 使用提取的 commit hash 通过 go get 获取 tfo-go
+go get gorm.io/gorm@$xray_commit_hash
