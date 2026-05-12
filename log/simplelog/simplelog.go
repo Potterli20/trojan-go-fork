dc4aa56a3617e075758b82861f7671afe1974aa0
trli <potter@trli.club>
2026-05-13 00:00:52 +0800

    refactor(simplelog): 为SimpleLogger添加内置logger实例支持
    通过新增logger字段，将日志输出委托给内置的golog.Logger实例，保留原有直接调用golog的降级逻辑，同时完善SetOutput方法参数命名和实现

--- log/simplelog/simplelog.go
+++ log/simplelog/simplelog.go
@@ -16,6 +16,7 @@ func init() {
 
 type SimpleLogger struct {
 	logLevel log.LogLevel
+	logger   *golog.Logger
 }
 
 func (l *SimpleLogger) SetLogLevel(level log.LogLevel) {
@@ -24,75 +25,123 @@ func (l *SimpleLogger) SetLogLevel(level log.LogLevel) {
 
 func (l *SimpleLogger) Fatal(v ...any) {
 	if l.logLevel <= log.FatalLevel {
-		golog.Fatal(sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Fatalln(sanitizeLogInput(v)...)
+		} else {
+			golog.Fatal(sanitizeLogInput(v)...)
+		}
 	}
 	os.Exit(1)
 }
 
 func (l *SimpleLogger) Fatalf(format string, v ...any) {
 	if l.logLevel <= log.FatalLevel {
-		golog.Fatalf(sanitizeString(format), sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Fatalf(sanitizeString(format), sanitizeLogInput(v)...)
+		} else {
+			golog.Fatalf(sanitizeString(format), sanitizeLogInput(v)...)
+		}
 	}
 	os.Exit(1)
 }
 
 func (l *SimpleLogger) Error(v ...any) {
 	if l.logLevel <= log.ErrorLevel {
-		golog.Println(sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Println(sanitizeLogInput(v)...)
+		} else {
+			golog.Println(sanitizeLogInput(v)...)
+		}
 	}
 }
 
 func (l *SimpleLogger) Errorf(format string, v ...any) {
 	if l.logLevel <= log.ErrorLevel {
-		golog.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		} else {
+			golog.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		}
 	}
 }
 
 func (l *SimpleLogger) Warn(v ...any) {
 	if l.logLevel <= log.WarnLevel {
-		golog.Println(sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Println(sanitizeLogInput(v)...)
+		} else {
+			golog.Println(sanitizeLogInput(v)...)
+		}
 	}
 }
 
 func (l *SimpleLogger) Warnf(format string, v ...any) {
 	if l.logLevel <= log.WarnLevel {
-		golog.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		} else {
+			golog.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		}
 	}
 }
 
 func (l *SimpleLogger) Info(v ...any) {
 	if l.logLevel <= log.InfoLevel {
-		golog.Println(sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Println(sanitizeLogInput(v)...)
+		} else {
+			golog.Println(sanitizeLogInput(v)...)
+		}
 	}
 }
 
 func (l *SimpleLogger) Infof(format string, v ...any) {
 	if l.logLevel <= log.InfoLevel {
-		golog.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		} else {
+			golog.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		}
 	}
 }
 
 func (l *SimpleLogger) Debug(v ...any) {
 	if l.logLevel <= log.AllLevel {
-		golog.Println(sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Println(sanitizeLogInput(v)...)
+		} else {
+			golog.Println(sanitizeLogInput(v)...)
+		}
 	}
 }
 
 func (l *SimpleLogger) Debugf(format string, v ...any) {
 	if l.logLevel <= log.AllLevel {
-		golog.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		} else {
+			golog.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		}
 	}
 }
 
 func (l *SimpleLogger) Trace(v ...any) {
 	if l.logLevel <= log.AllLevel {
-		golog.Println(sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Println(sanitizeLogInput(v)...)
+		} else {
+			golog.Println(sanitizeLogInput(v)...)
+		}
 	}
 }
 
 func (l *SimpleLogger) Tracef(format string, v ...any) {
 	if l.logLevel <= log.AllLevel {
-		golog.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		if l.logger != nil {
+			l.logger.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		} else {
+			golog.Printf(sanitizeString(format), sanitizeLogInput(v)...)
+		}
 	}
 }
 
@@ -126,5 +175,6 @@ func sanitizeString(s string) string {
 	return s
 }
 
-func (l *SimpleLogger) SetOutput(io.Writer) {
+func (l *SimpleLogger) SetOutput(w io.Writer) {
+	l.logger = golog.New(w, "", 0)
 }
