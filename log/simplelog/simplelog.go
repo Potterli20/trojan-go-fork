65104d0cf4ef255292366a6a17a0280ceebd8b32
trli <potter@trli.club>
2026-06-18 07:52:49 +0800

    fix(simplelog): 修复日志级别判断逻辑，调整日志输出流程
    重构了所有日志方法的判断和执行流程，将日志级别检查前置，先判断是否需要跳过日志输出，再统一处理日志内容的脱敏和输出，同时修正了Fatal级别的退出逻辑位置。

--- log/simplelog/simplelog.go
+++ log/simplelog/simplelog.go
@@ -22,142 +22,154 @@ func (l *SimpleLogger) SetLogLevel(level log.LogLevel) {
 }
 
 func (l *SimpleLogger) Fatal(v ...any) {
-	if l.logLevel <= log.FatalLevel {
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Fatalln(sanitized...)
-		} else {
-			golog.Fatal(sanitized...)
-		}
+	if l.logLevel > log.FatalLevel {
+		os.Exit(1)
+	}
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Fatalln(sanitized...)
+	} else {
+		golog.Fatal(sanitized...)
 	}
 	os.Exit(1)
 }
 
 func (l *SimpleLogger) Fatalf(format string, v ...any) {
-	if l.logLevel <= log.FatalLevel {
-		sanitizedFormat := log.SanitizeString(format)
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Fatalf(sanitizedFormat, sanitized...)
-		} else {
-			golog.Fatalf(sanitizedFormat, sanitized...)
-		}
+	if l.logLevel > log.FatalLevel {
+		os.Exit(1)
+	}
+	sanitizedFormat := log.SanitizeString(format)
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Fatalf(sanitizedFormat, sanitized...)
+	} else {
+		golog.Fatalf(sanitizedFormat, sanitized...)
 	}
 	os.Exit(1)
 }
 
 func (l *SimpleLogger) Error(v ...any) {
-	if l.logLevel <= log.ErrorLevel {
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Println(sanitized...)
-		} else {
-			golog.Println(sanitized...)
-		}
+	if l.logLevel > log.ErrorLevel {
+		return
+	}
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Println(sanitized...)
+	} else {
+		golog.Println(sanitized...)
 	}
 }
 
 func (l *SimpleLogger) Errorf(format string, v ...any) {
-	if l.logLevel <= log.ErrorLevel {
-		sanitizedFormat := log.SanitizeString(format)
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Printf(sanitizedFormat, sanitized...)
-		} else {
-			golog.Printf(sanitizedFormat, sanitized...)
-		}
+	if l.logLevel > log.ErrorLevel {
+		return
+	}
+	sanitizedFormat := log.SanitizeString(format)
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Printf(sanitizedFormat, sanitized...)
+	} else {
+		golog.Printf(sanitizedFormat, sanitized...)
 	}
 }
 
 func (l *SimpleLogger) Warn(v ...any) {
-	if l.logLevel <= log.WarnLevel {
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Println(sanitized...)
-		} else {
-			golog.Println(sanitized...)
-		}
+	if l.logLevel > log.WarnLevel {
+		return
+	}
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Println(sanitized...)
+	} else {
+		golog.Println(sanitized...)
 	}
 }
 
 func (l *SimpleLogger) Warnf(format string, v ...any) {
-	if l.logLevel <= log.WarnLevel {
-		sanitizedFormat := log.SanitizeString(format)
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Printf(sanitizedFormat, sanitized...)
-		} else {
-			golog.Printf(sanitizedFormat, sanitized...)
-		}
+	if l.logLevel > log.WarnLevel {
+		return
+	}
+	sanitizedFormat := log.SanitizeString(format)
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Printf(sanitizedFormat, sanitized...)
+	} else {
+		golog.Printf(sanitizedFormat, sanitized...)
 	}
 }
 
 func (l *SimpleLogger) Info(v ...any) {
-	if l.logLevel <= log.InfoLevel {
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Println(sanitized...)
-		} else {
-			golog.Println(sanitized...)
-		}
+	if l.logLevel > log.InfoLevel {
+		return
+	}
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Println(sanitized...)
+	} else {
+		golog.Println(sanitized...)
 	}
 }
 
 func (l *SimpleLogger) Infof(format string, v ...any) {
-	if l.logLevel <= log.InfoLevel {
-		sanitizedFormat := log.SanitizeString(format)
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Printf(sanitizedFormat, sanitized...)
-		} else {
-			golog.Printf(sanitizedFormat, sanitized...)
-		}
+	if l.logLevel > log.InfoLevel {
+		return
+	}
+	sanitizedFormat := log.SanitizeString(format)
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Printf(sanitizedFormat, sanitized...)
+	} else {
+		golog.Printf(sanitizedFormat, sanitized...)
 	}
 }
 
 func (l *SimpleLogger) Debug(v ...any) {
-	if l.logLevel <= log.AllLevel {
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Println(sanitized...)
-		} else {
-			golog.Println(sanitized...)
-		}
+	if l.logLevel > log.AllLevel {
+		return
+	}
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Println(sanitized...)
+	} else {
+		golog.Println(sanitized...)
 	}
 }
 
 func (l *SimpleLogger) Debugf(format string, v ...any) {
-	if l.logLevel <= log.AllLevel {
-		sanitizedFormat := log.SanitizeString(format)
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Printf(sanitizedFormat, sanitized...)
-		} else {
-			golog.Printf(sanitizedFormat, sanitized...)
-		}
+	if l.logLevel > log.AllLevel {
+		return
+	}
+	sanitizedFormat := log.SanitizeString(format)
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Printf(sanitizedFormat, sanitized...)
+	} else {
+		golog.Printf(sanitizedFormat, sanitized...)
 	}
 }
 
 func (l *SimpleLogger) Trace(v ...any) {
-	if l.logLevel <= log.AllLevel {
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Println(sanitized...)
-		} else {
-			golog.Println(sanitized...)
-		}
+	if l.logLevel > log.AllLevel {
+		return
+	}
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Println(sanitized...)
+	} else {
+		golog.Println(sanitized...)
 	}
 }
 
 func (l *SimpleLogger) Tracef(format string, v ...any) {
-	if l.logLevel <= log.AllLevel {
-		sanitizedFormat := log.SanitizeString(format)
-		sanitized := log.SanitizeLogInput(v)
-		if l.logger != nil {
-			l.logger.Printf(sanitizedFormat, sanitized...)
-		} else {
-			golog.Printf(sanitizedFormat, sanitized...)
-		}
+	if l.logLevel > log.AllLevel {
+		return
+	}
+	sanitizedFormat := log.SanitizeString(format)
+	sanitized := log.SanitizeLogInput(v)
+	if l.logger != nil {
+		l.logger.Printf(sanitizedFormat, sanitized...)
+	} else {
+		golog.Printf(sanitizedFormat, sanitized...)
 	}
 }
 
